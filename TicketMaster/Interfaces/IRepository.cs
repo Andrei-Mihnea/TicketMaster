35891fc04b4931b35f330b1710525efdccda989b
Andrei-Mihnea <andrei.mihnea@student.tuiasi.ro>
2025-10-21 10:44:17 +0300

    Finished implementation of the interface

--- TicketMaster/Interfaces/IRepository.cs
+++ TicketMaster/Interfaces/IRepository.cs
@@ -1,7 +1,12 @@
 namespace Interfaces
 {
-    public interface IRepository<T>
+    public interface IRepository<T>//interface that will contain some CRUD operations for the stocking of data
     {
-        
+        public void AddMember(T member);//Create
+        public IList<T> GetAllMembers();//Read
+        public void Update(T member);//Update
+        public void Delete(T member);//Delete
+
+
     }
 }
