ea7c50b203fdddac6178bf6c0c49c9c81bb0f075
Andrei-Mihnea <andrei.mihnea@student.tuiasi.ro>
2025-10-21 13:53:57 +0300

    nothing really to commit, working tree clean

--- TicketMaster/Database/BiletPachetEvenimenteDb.cs
+++ TicketMaster/Database/BiletPachetEvenimenteDb.cs
@@ -5,6 +5,7 @@ namespace Database
 {
     public class BiletPacheteEvenimenteDb : IRepository<BiletPacheteEvenimenteModel>//Number 1
     {
+        
         public void AddMember(BiletPacheteEvenimenteModel member)
         {
             throw new NotImplementedException();
